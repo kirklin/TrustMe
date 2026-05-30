7d0998b95f81a42119174d990a9bfe28072cf9d5
Kirk Lin <linkirk@163.com>
2026-05-30 19:57:13 +0800

    chore: bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "hk.kirk.trustme"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.1.0"
+        versionCode = 9
+        versionName = "1.2.0"
     }
 
     signingConfigs {
