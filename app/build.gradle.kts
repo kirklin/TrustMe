fdad11f3b22cdea7cd2dfca4f6e274aac3bebbaf
Kirk Lin <linkirk@163.com>
2026-05-30 16:49:12 +0800

    release: v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "hk.kirk.trustme"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.1.0"
     }
 
     signingConfigs {
